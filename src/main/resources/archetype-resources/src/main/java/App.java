232220ea20c6f4abb7a667c85aa61f9e88f929f6
jvcmtr <jvictorcmtr@gmail.com>
2023-11-23 19:22:09 -0300

    Init Maven

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $INFNET.CICERO.MAVEN_TP1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
